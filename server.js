84593037d9da8d20883f89332d184d5b860ead62
jscudClassLaptop <jscudereweiss@clakru.edu>
2022-04-13 10:00:40 -0400

    Jonah alapaca Facts & header modifications

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
+/*app.use(express.static(__dirname + "/public/photos"));*/
 
 // =============================================================
 
