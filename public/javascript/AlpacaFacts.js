b3eb98c09d55d55c805cfaca2478cf0248c8eb67
jscudMainDevice <jscudweiss@gmail.com>
2022-04-24 14:34:10 -0400

    working creating image loader

--- public/javascript/AlpacaFacts.js
+++ public/javascript/AlpacaFacts.js
@@ -4,7 +4,7 @@ $('#factDetail').empty();
 function get_fact_List(fact) {
     return `
         <div class="row">
-            <button type="button" class="btn btn-success fact-btn" value="${fact.Info} ">${fact.Title}</button>
+            <button type="button" class="btn btn-success fact-btn" value="${fact.Info}" id="${fact.url}">${fact.Title}</button>
         </div>
         <br>
     `
@@ -13,6 +13,9 @@ function get_fact_List(fact) {
 
 $.getJSON("/data/Alpaca-Facts.json", () => {
 }).done((data) => {
+    $('#factDetail').text(data[0].Info);
+    $('#factTitle').text(data[0].Title);
+    $('#factPic').attr('src',data[0].url);
     console.log(data);
     data.forEach((alpFact) => {
         $('#factNameList').append(() => {
@@ -21,8 +24,12 @@ $.getJSON("/data/Alpaca-Facts.json", () => {
     })
     $(".fact-btn").click(function () {
         let factText = $(this).attr('value');
+        let factTitle = $(this).text();
+        let factUrl = $(this).attr("id");
         console.log(factText);
         $('#factDetail').text(factText);
+        $('#factTitle').text(factTitle);
+        $('#factPic').attr('src',factUrl);
     })
 })
 
