5683ca4b8d8bdf704cf8a96c72ac255101393361
jscudMainDevice <jscudweiss@gmail.com>
2022-04-30 21:40:01 -0400

    calendar with test events

--- public/javascript/calendar.js
+++ public/javascript/calendar.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
+        googleCalendarApiKey: 'AIzaSyAPRI9sO3Jq1sDPEgz5ply9kMiQib5ZZLY',
         themeSystem: 'bootstrap5',
         initialView: 'dayGridMonth',
         headerToolbar: {
@@ -9,6 +10,9 @@ document.addEventListener('DOMContentLoaded', function() {
             center: 'title',
             right: 'dayGridMonth,timeGridWeek,timeGridDay'
         },
+        events: {
+            googleCalendarId: 'ag2c5620rn7vnm9hslau2veokk@group.calendar.google.com'
+        }
     });
 
     calendar.render();
